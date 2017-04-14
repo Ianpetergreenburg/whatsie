682b6805d1cf445e446c6eea2a5212f0bd994744
Ian Peter Greenburg <email": null>
2017-04-13 19:44:52 -0700

    new redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse("redis://redistogo:0d2e89b44411640b145171920259f26f@barreleye.redistogo.com:10982/" || "redis://localhost:6379/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
+REDIS = Redis.new(:url => uri)
