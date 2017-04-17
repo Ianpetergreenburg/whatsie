3bbe5ca3f0b3c4ae03180a94c1861815908a8b29
Ian Peter Greenburg <email": null>
2017-04-17 14:13:00 -0700

    dynamic changes to job loading

--- app/jobs/job_loader_scraper_job.rb
+++ app/jobs/job_loader_scraper_job.rb
@@ -2,8 +2,13 @@ class JobLoaderScraperJob < ApplicationJob
   self.queue_adapter = :sidekiq
   queue_as :high_priority
 
+  scrapes = ENV['SCRAPER_COUNT'] || 10
+  @scrapes = scrapes.to_i
+
+
+
   def perform
-    RecipeUrl.next_to_scrape(10).each do |recipe_url|
+    RecipeUrl.next_to_scrape(@scrapes).each do |recipe_url|
       NytScraperJob.perform_later(recipe_url)
     end
   end
