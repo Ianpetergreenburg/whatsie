b2f6390eba7ccd57496ea97c3c846729ddf8d155
Ian Peter Greenburg <email": null>
2017-04-18 19:41:11 -0700

    loader constant establishment

--- app/jobs/job_loader_starter_job.rb
+++ app/jobs/job_loader_starter_job.rb
@@ -1,14 +1,15 @@
 class JobLoaderStarterJob < ApplicationJob
   self.queue_adapter = :sidekiq
   queue_as :low_priority
+  @@nyt = 1
 
   def perform
     loads = ENV['LOADER_COUNT'] || 10
     @loads = loads.to_i
 
     @loads.times do |i|
-      i += RecipeUrl.last.recipe_id if RecipeUrl.count > 0
-      UrlLoaderJob.perform_later('https://cooking.nytimes.com/recipes/' + i.to_s, 'nyt', i)
+      UrlLoaderJob.perform_later('https://cooking.nytimes.com/recipes/' + @@nyt.to_s, 'nyt', @@nyt)
+      @@nyt += 1
     end
   end
 end
